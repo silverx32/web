d522eac82a3c825e6c8d678957879d1e8402fb2f
不言 <2498121302@qq.com>
2022-01-27 16:51:08 +0800

    第二次

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from typing import List, Optional
+from typing import Optional
 
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-from datetime import datetime
-from enum import Enum
-import aiofiles
-
-if __name__ == '__main__':
-    uvicorn.run(app='main:app', host='127.0.0.1', port=8000, reload=True)
 
 app = FastAPI()
 
@@ -22,6 +16,8 @@ acc = {
         'pw': 133233,
         'level': 1}
 }
+
+post_item = {'fastapi': {'title': 'fastapi', 'body': 'nice'}}
 
 
 class User(BaseModel):
@@ -49,13 +45,54 @@ async def login(user: User):
 
 @app.post('/register')
 async def register(user: User):
-    a = {}
     if user.id in acc:
         return '账号已存在'
     else:
-        acc[user.id] = a
-        a['name', 'pw', 'level'] = [user.name, user.pw, 1]
+        acc[user.id] = {'name': user.name, 'pw': user.pw, 'level': 1}
         print(acc)
         return '注册成功'
 
 
+class Post(BaseModel):
+    title: str
+    body: str
+
+
+@app.post('/post')
+async def post_in(post: Post):
+    post_item[post.title] = {'title':post.title ,'body':post.body}
+    print(post_item[post.title])
+    return '发帖成功'
+
+
+@app.get('/post/{post_title}')
+async def post_add(post_title: str):
+    if post_title in post_item:
+        return post_item[post_title]['body']
+    pass
+
+
+@app.delete('/users/{id}')
+def delete_user(userid:int):
+    if userid in acc:
+        del acc[userid]
+        return '已完成'
+    else:
+        raise HTTPException(status_code=404, detail="账号不存在")
+
+
+@app.put('/users/{id}')
+def update_pw(userid: int, name: Optional[str] = None, pw: Optional[int] = None):
+    if userid in acc:
+        if name is not None:
+            acc[userid]['name'] = name
+        if pw is not None:
+            acc[userid]['pw'] = pw
+        print(acc)
+        return '已完成'
+    else:
+        raise HTTPException(status_code=404, detail="账号不存在")
+
+
+if __name__ == '__main__':
+    uvicorn.run(app='main:app', host='127.0.0.1', port=8000, reload=True)
