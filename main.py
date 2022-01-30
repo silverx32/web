951527a5c982d352c3324d31af3848fb3681652a
不言 <2498121302@qq.com>
2022-01-30 20:35:35 +0800

    第五次，setcookie，

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Cookie, Response, Header, Request
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -10,13 +10,11 @@ acc = {
     1: {
         'name': 'jsy',
         'pw': 538364,
-        'level': 1,
-        'login': 0},
+        'level': 1, },
     2: {
         'name': 'zyh',
         'pw': 133233,
-        'level': 1,
-        'login': 0}
+        'level': 1, }
 }
 
 post_item = {
@@ -47,32 +45,27 @@ class Post(BaseModel):
     body: str
 
 
-def check_login(userid: int):
-    if userid in acc and acc[userid]['login'] == 1:
-        return True
-    else:
-        raise HTTPException(status_code=233, detail='请登录')
-
-
 @app.get('/')  # 首页
 async def root():
     return 'Welcome!'
 
 
 @app.post('/login')  # 登录
-async def login(user: User):
+async def login(user: User, response: Response):
     if user.id in acc:
-        if acc[user.id]['login'] == 1:
-            return '已经登录'
+        print(user)
+        if user.pw == acc[user.id]['pw']:
+            await set_cookie(response, acc[user.id]['name'])
+            return '登陆成功'
         else:
-            print(user)
-            if user.pw == acc[user.id]['pw']:
-                acc[user.id]['login'] = 1
-                return '登陆成功,登录码login=%s' % acc[user.id]['login']
-            else:
-                return '密码错误'
+            return '密码错误'
     else:
         return '无此账号'
+
+
+async def set_cookie(response: Response, user_name):
+    response.set_cookie(key=user_name, value='login')
+    return {'message': 'welcome'}
 
 
 @app.post('/{userid}logout')  # 登出
@@ -168,6 +161,13 @@ def post_comment(userid: int, post_title: str, post_comment_title: str, post_com
             post_item[post_title]['post_comment'] = {post_comment_title: post_comment_body}
             print(post_item[post_title]['post_comment'])
             return '评论成功'
+
+
+def check_login(userid: int):
+    if userid in acc and acc[userid]['login'] == 1:
+        return True
+    else:
+        raise HTTPException(status_code=233, detail='请登录')
 
 
 if __name__ == '__main__':
